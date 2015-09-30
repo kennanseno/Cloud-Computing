f91cc19ea359381261d7ddb336294a69988fad9f
Kennan Seno <k_seno@yahoo.com>
2015-09-30 11:06:15 +0000

    added comments

--- Lab3/palindrome.py
+++ Lab3/palindrome.py
@@ -1,7 +1,7 @@
 myString = raw_input("Enter a String: ")
-
+# reverse the string
 revString = reversed(myString)
-
+# check if the string is equal to its reverse
 if list(myString) == list(revString):
    print("True")
 else:
